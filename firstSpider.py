6ebf21843ece9a1ddd661416f1a8b2c5ad8309c5
wxj630 <717366564@qq.com>
2018-11-19 23:08:48 +0800

    add chinese info

--- firstSpider.py
+++ firstSpider.py
@@ -26,7 +26,7 @@ r= requests.get('http://httpbin.org/get',params= key_dict)
 print("url已经正确编码：",r.url)
 print("字符串方式的响应体：\n",r.text)
 
-# post request to be safer than get request.
+# post request to be safer than get request.post方式比get方式更安全
 key_dict2= {'key1':'value1','key2':'value2'}
 r = requests.post('http://httpbin.org/post',data=key_dict2)
 print(r.text)
