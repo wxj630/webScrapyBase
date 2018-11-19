d27c2478f8c6005da07e9ed5bd23c6585e94ab4e
wxj630 <wxj630@foxmail.com>
2018-11-19 17:30:40 +0800

    firstSpider-post

--- firstSpider.py
+++ firstSpider.py
@@ -25,3 +25,8 @@ key_dict= {'key1':'value1','key2':'value2'}
 r= requests.get('http://httpbin.org/get',params= key_dict)
 print("url已经正确编码：",r.url)
 print("字符串方式的响应体：\n",r.text)
+
+# post request to be safer than get request
+key_dict2= {'key1':'value1','key2':'value2'}
+r = requests.post('http://httpbin.org/post',data=key_dict2)
+print(r.text)
