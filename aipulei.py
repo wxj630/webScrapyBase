a6475fc59a0b6d809845e5513963c4d0885496d9
wxj630 <wxj630@foxmail.com>
2018-11-25 17:13:04 +0800

    aipulei success

--- aipulei.py
+++ aipulei.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
-import re
-import time
 
 driver = webdriver.Firefox()
-for i in range(1,9):
+for i in range(35210,48898):
 
-    driver.get("http://retractiondatabase.com/撤稿查询/aid/22221"+str(i)+"/mid/447")
-    url=str("http://retractiondatabase.com/撤稿查询/aid/22221"+str(i)+"/mid/447")
+    driver.get("http://retractiondatabase.com/撤稿查询/aid/2"+str(i)+"/mid/447")
+    url=str("http://retractiondatabase.com/撤稿查询/aid/2"+str(i)+"/mid/447")
 
     tit = driver.find_element_by_id('article_title')
     content = tit.find_element_by_tag_name('h1')
@@ -17,21 +15,20 @@ for i in range(1,9):
     url2 = str(content3)
 
     infos = driver.find_elements_by_class_name('list-group-item-text')
-    with open('aipulei.txt', 'a+', encoding='utf-8') as f:
+    with open('aipulei5.txt', 'a+', encoding='utf-8') as f:
         f.write(title+"#"+url+"#"+url2+"#")
-        f.close()
+
 
     for info in infos:
         if info.text is None:
             content2 = "N/A"
         else:
             content2 = info.text
-        with open('aipulei.txt', 'a+', encoding='utf-8') as f:
+        with open('aipulei5.txt', 'a+', encoding='utf-8') as f:
             f.write(content2+"#")
-            f.close()
-    with open('aipulei.txt', 'a+', encoding='utf-8') as f:
+
+    with open('aipulei5.txt', 'a+', encoding='utf-8') as f:
         f.write("\n")
-        f.close()
 
 
 
